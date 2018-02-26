9cb3f2b148b224694bad2dd329c89c3478627ed8
Ammar Alasadi <aalasadi@adminisatorsmbp.hd.corp>
2018-02-26 17:06:08 -0500

    added other comps and learning material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,15 +5,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  post = {
-    title: "The book of Kusha",
-    isFavorite: true
+  // post = {
+  //   title: "The book of Kusha",
+  //   isFavorite: true
+  // }
+  // onFavoriteChange(evetArgs) {
+  //   console.log('favorite changed', evetArgs);
+  // }
+  // tweet = {
+  //   likeCount: 10,
+  //   isLiked: true
+  // }
+  // cources = [1,2,3]
+
+  // viewMode = '';
+  cources;
+  // onAdd(){
+  //   this.cources.push({ id:4, name: "new cource" + 1 });
+  // }
+  onChange(cource) {
+    let index = this.cources.indexOf(cource);
+    this.cources.splice(index, 1);
   }
-  onFavoriteChange(evetArgs) {
-    console.log('favorite changed', evetArgs);
+  loadCources() {
+    this.cources = [
+      { id: 1, name: 'course1' },
+      { id: 2, name: 'course2' },
+      { id: 3, name: 'course3' }
+    ]
   }
-  tweet = {
-    likeCount: 10,
-    isLiked: true
+  trackCource(index,cource){
+    return cource ? cource.id: undefined;
   }
 }
--- src/app/cource/cource.component.ts
+++ src/app/cource/cource.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cource',
+  selector: 'cource',
   templateUrl: './cource.component.html',
   styleUrls: ['./cource.component.css']
 })
